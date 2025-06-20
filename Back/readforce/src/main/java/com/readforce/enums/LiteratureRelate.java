a014400e2aa4d2bfd8d6ba07b262609325e0368a
KimJeHyun91 <kjh910920@gmail.com>
2025-06-20 14:52:40 +0900

    6.20 2:52
    - 실행 오류 해결

---
+++ Back/readforce/src/main/java/com/readforce/enums/LiteratureRelate.java
@@ -0,0 +1,13 @@
+package com.readforce.enums;
+
+public enum LiteratureRelate {
+
+	;
+	
+	public static enum type {
+		
+		NOVEL, FAIRYTALE
+		
+	}
+	
+}
--- Back/readforce/src/main/java/com/readforce/repository/NewsRepository.java
+++ Back/readforce/src/main/java/com/readforce/repository/NewsRepository.java
@@ -14,17 +14,17 @@ import com.readforce.entity.News;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long>{
 	
-	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'beginner' ORDER BY RAND() LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'beginner' ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
 	Optional<GetNews> findByLanguageAndBeginnerRandom(
 			@Param("language") String language
 	);
 
-	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'intermediate' ORDER BY RAND() LIMIT 2", nativeQuery = true)
+	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'intermediate' ORDER BY RANDOM() LIMIT 2", nativeQuery = true)
 	List<GetNews> findByLanguageAndIntermediateRandom(
 			@Param("language") String language
 	);
 
-	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'advanced' ORDER BY RAND() LIMIT 2", nativeQuery = true)
+	@Query(value = "SELECT * FROM news WHERE language = :language AND level = 'advanced' ORDER BY RANDOM() LIMIT 2", nativeQuery = true)
 	List<GetNews> findByLanguageAndAdvancedRandom(
 			@Param("language") String language
 	);
