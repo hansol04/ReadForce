2aa18f00fc5d410fc9562da87e27fd179ecf68f1
KimJeHyun91 <kjh910920@gmail.com>
2025-06-20 15:20:53 +0900

    6.20 3:20
    - 실행오류 해결

--- Back/readforce/src/main/java/com/readforce/repository/LiteratureParagraphRepository.java
+++ Back/readforce/src/main/java/com/readforce/repository/LiteratureParagraphRepository.java
@@ -1,12 +1,23 @@
 package com.readforce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.readforce.dto.LiteratureDto.GetLiteratureParagraph;
 import com.readforce.entity.LiteratureParagraph;
 import com.readforce.id.LiteratureParagraphId;
 
 @Repository
 public interface LiteratureParagraphRepository extends JpaRepository<LiteratureParagraph, LiteratureParagraphId>{
+
+//	@Query(value = "SELECT lp FROM LiteratureParagraph lp WHERE (SELECT l.literature_no FROM Literature l WHERE type = :type)")
+//	List<GetLiteratureParagraph> getLiteratureParagraphListByTypeOrderByAsc(@Param("type") String type);
+//
+//	
+//	List<GetLiteratureParagraph> getLiteratureParagraphListByTypeOrderByDesc(String type);
 
 }
