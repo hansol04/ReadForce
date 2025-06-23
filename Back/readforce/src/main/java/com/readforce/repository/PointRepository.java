018dc7d7fb065c5fbf4d9d83df953161bf36ea51
KimJeHyun91 <kjh910920@gmail.com>
2025-06-23 13:48:02 +0900

    6.23 1:47
    - 관리자 기능 대폭 추가
    - 랭킹 기능 추가

--- Back/readforce/src/main/java/com/readforce/repository/PointRepository.java
+++ Back/readforce/src/main/java/com/readforce/repository/PointRepository.java
@@ -1,5 +1,6 @@
 package com.readforce.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,6 +24,21 @@ public interface PointRepository extends JpaRepository<Point, String>{
 	void deleteByEmail(
 			@Param("email") String email
 	);
+
+	@Query(value = "SELECT * FROM point ORDER BY korean_news DESC LIMIT 50", nativeQuery = true)
+	List<Point> findAllWithKoreanNews();
+
+	@Query(value = "SELECT * FROM point ORDER BY english_news DESC LIMIT 50", nativeQuery = true)
+	List<Point> findAllWithEnglishNews();
+
+	@Query(value = "SELECT * FROM point ORDER BY japanese_news DESC LIMIT 50", nativeQuery = true)
+	List<Point> findAllWithJapaneseNews();
+
+	@Query(value = "SELECT * FROM point ORDER BY novel DESC LIMIT 50", nativeQuery = true)
+	List<Point> findAllWithNovel();
+
+	@Query(value = "SELECT * FROM point ORDER BY fairytale DESC LIMIT 50", nativeQuery = true)
+	List<Point> findAllWithFairytale();
 
 
 }
