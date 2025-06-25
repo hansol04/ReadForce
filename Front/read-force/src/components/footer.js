cace7b48b9fb0c14cb82be281b0eb9d7f66a08c6
hansol04 <gksthf0428@naver.com>
2025-06-25 11:27:20 +0900

    푸터변경

--- Front/read-force/src/components/footer.js
+++ Front/read-force/src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="page-container">
-                <h3 className="footer-title">오늘의 문해력</h3>
+                <h3 className="footer-title">리드 포스</h3>
                 <p>서울 강남구 강남대로78길 8 한국빌딩 8층</p>
                 <p>상담시간 9:00~18:00 (점심시간 12:50~13:50)</p>
             </div>
